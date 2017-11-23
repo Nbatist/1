2d621423e089eb52222b211c7cfd1197d4d723a0
Nbatist <33941407+Nbatist@users.noreply.github.com>
2017-11-23 23:06:50 +0200

    Update Main.java

--- TRSPO/src/Lab1/Main.java
+++ TRSPO/src/Lab1/Main.java
@@ -3,7 +3,7 @@ package Lab1;
 import java.util.Scanner;
 
 /**
- * Created by Nick Batist on 09/23/17.
+ * Created by Nick Batist on 11/23/17.
  */
 public class Main {
     public static void main(String[] args) {
